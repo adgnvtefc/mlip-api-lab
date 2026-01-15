48a4365beb4c0643b48d151a426bfcb313b295fb
adgnvtefc <zhang.hanmo2018@gmail.com>
2026-01-15 13:20:33 -0500

    make changes for the lab

--- analyze.py
+++ analyze.py
@@ -2,11 +2,14 @@ import json
 import os
 from typing import Any, Dict
 from litellm import completion
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # You can replace these with other models as needed but this is the one we suggest for this lab.
-MODEL = "groq/llama-3.3-70b-versatile"
-
-api_key = "hardcoded API_KEY HERE"
+MODEL = "gpt-4o-mini"
 
 
 def get_itinerary(destination: str) -> Dict[str, Any]:
@@ -21,7 +24,21 @@ def get_itinerary(destination: str) -> Dict[str, Any]:
 
     # See https://docs.litellm.ai/docs/ for reference.
 
-    data = ...
+    class DestinationTingy(BaseModel):
+        destination: str
+        price_range: str
+        ideal_visit_times: list[str]
+        top_attractions: list[str]
     
+    messages= [{"role": "system", "content": "You are a travel assistant that provides detailed travel itineraries."},
+               {"role": "user", "content": f"Create a travel itinerary for {destination} including price range, ideal visit times, and top attractions."}]
 
+    response = completion(
+        model=MODEL,
+        response_format=DestinationTingy,
+        messages=messages
+    )
+
+    data = response.choices[0].message.content
+    
     return data
